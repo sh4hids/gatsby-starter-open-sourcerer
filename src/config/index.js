69ec0a150869b419aac3706d91c008b026a5adc0
sh4hids <dev@shahid.pro>
2021-08-17 09:31:22 +0600

    ↕ update config

--- src/config/index.js
+++ src/config/index.js
@@ -1,11 +1,10 @@
 const config = {
-  title: 'Gatsby Theme Open Sourcerer',
-  shortTitle: 'HackerMan',
-  description: 'A minimal gatsby theme for open source developer',
+  siteTitle: 'Gatsby Theme Open Sourcerer',
+  siteDescription: 'A minimal gatsby theme for open source developer',
   siteUrl: 'https://example.com/',
   contentPath: 'contents',
-  logo: '/logo.jpg',
-  seoImage: '/seo.jpg',
+  headerLogoText: 'HackerMan',
+  seoImage: '/images/seo.jpg',
   heroImage: '',
   titleSeparator: '|',
   pathPrefix: '/',
@@ -20,24 +19,24 @@ const config = {
   },
   author: {
     fullName: 'Denver Coder 9',
+    nickName: 'HackerMan',
     bio: 'Full-time open sourcerer',
     summary:
       'Eu nulla labore adipisicing consequat veniam qui ullamco. Fugiat laborum consequat elit enim quis ullamco sit minim dolor ad.',
     email: 'hello@example.com',
-    skype: 'denvercoder9',
     links: {
-      facebook: 'https://facebook.com/denvercoder9',
-      github: 'https://github.com/denvercoder9',
-      instagram: 'https://instagram.com/denvercoder9',
-      linkedin: 'https://linkedin.com/denvercoder9',
-      twitter: 'https://twitter.com/denvercoder9',
+      facebook: 'https://facebook.com/danvercoder9',
+      github: 'https://github.com/danvercoder9',
+      instagram: 'https://instagram.com/danvercoder9',
+      linkedin: 'https://linkedin.com/danvercoder9',
+      twitter: 'https://twitter.com/danvercoder9',
     },
   },
   footer: {
     title: 'Words of Thanks',
     description:
       'Sunt adipisicing culpa deserunt est est excepteur voluptate occaecat voluptate irure elit sit nostrud culpa. Deserunt est est excepteur voluptate occaecat voluptate irure elit sit',
-    copyright: `© ${new Date().getFullYear()} Denver Coder 9. All Rights Reserved.`,
+    copyright: `© ${new Date().getFullYear()} Danver Coder. All Rights Reserved.`,
   },
 };
 
